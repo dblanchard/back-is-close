a92de4ea3da6834891bffe1da232c0bdb7424e3a
Duane Blanchard <dblanchard@gmail.com>
2025-05-19 13:42:53 -0700

    Almost there.... Stay on target....

--- background.js
+++ background.js
@@ -9,18 +9,13 @@ chrome.commands.onCommand.addListener((command) => {
   if (command === 'check-back-and-close') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: checkAndCloseTab
+      chrome.tabs.get(tab.id, (tabInfo) => {
+        if (tabInfo.url === "about:blank" || tabInfo.url === "about:newtab" || tabInfo.url === "about:home") {
+          chrome.tabs.remove(tab.id);
+        } else {
+          chrome.tabs.goBack(() => {});
+        }
       });
     });
   }
 });
-
-function checkAndCloseTab() {
-  if (window.history.length <= 1) {
-    window.close();
-  } else {
-    history.back();
-  }
-}
